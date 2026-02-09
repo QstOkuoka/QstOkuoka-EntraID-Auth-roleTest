a87f53f7b2bb1a1b8c9aca59764cca8f982f1a84
QstOkuoka <takuma.okuoka@quest.co.jp>
2026-02-09 15:29:40 +0900

    Create index.js

---
+++ api/getRoles/index.js
@@ -0,0 +1,18 @@
+
+const rolesFromEntra = req.body.claims
+  .filter(c => c.typ === "roles")
+  .map(c => c.val);
+
+const roles = [];
+if (rolesFromEntra.includes("Admin")) {
+  roles.push("Admin");
+}
+if (rolesFromEntra.includes("User")) {
+  roles.push("User");
+}
+
+context.res = {
+  status: 200,
+  body: { roles }
+};
+``
